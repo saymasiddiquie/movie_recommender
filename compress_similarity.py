cfef86631a4cc30eb3f517153ef837c5bcaf2daa
saymasiddiquie <sayma.siddiquie3@gmail.com>
2025-06-16 18:06:22 +0530

    Delete compress_similarity.py

--- compress_similarity.py
+++
@@ -1,9 +0,0 @@
-import gzip
-import pickle
-
-# Compress similarity.pkl to similarity.pkl.gz
-with open("similarity.pkl", "rb") as f_in:
-    with gzip.open("similarity.pkl.gz", "wb") as f_out:
-        f_out.writelines(f_in)
-
-print("✅ similarity.pkl has been compressed successfully to similarity.pkl.gz")
